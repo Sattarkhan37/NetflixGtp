efab3c81de14cf8585fbe7742ffd61886ba18cfe
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-03-24 14:59:43 +0530

    bug fix : tail wind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import React from "react";
 import "./app.css";
+
 const App = () => {
-  return <div>Hello</div>;
+  return <div className="text-3xl font-bold text-red-500">Hello</div>;
 };
 
 export default App;
