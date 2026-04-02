ee8b1c4ab26005414376b6e5153477d6fb0add0c
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-04-03 03:15:34 +0530

    successfully fetched data from TMDB

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { API_OPTIONS } from "../utils/constants";
 
 const Browse = () => {
+  const getNowPlayingMovie = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing",
+      API_OPTIONS,
+    );
+    const json = await data.json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getNowPlayingMovie();
+  }, []);
   return (
     <div>
       <Header />
