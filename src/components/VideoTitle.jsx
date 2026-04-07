e643cc3f59bc5c942faa9699e6732bab2519d455
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-04-08 02:33:44 +0530

    add background video player

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="py-15 px-2">
-      <h1 className="text-4xl px-2 font-bold py-2">{title}</h1>
-      <p className="py-2 px-2 text-lg w-1/2">{overview}</p>
+    <div className="w-screen aspect-video py-[15%] px-2 absolute text-amber-50 bg-gradient-to-r from-black">
+      <h1 className="text-3xl px-4 font-bold py-2">{title}</h1>
+      <p className="py-2 px-4 text-0.5xl w-1/2">{overview}</p>
       <div className="py-2 px-2">
-        <button className="rounded-lg py-2 px-2 cursor-pointer bg-gray-300 w-25 text-black">
-        ⏵  Play
+        <button className="rounded-lg py-2 px-2 cursor-pointer bg-gray-300 w-25 text-black hover:bg-gray-300/30">
+          ⏵ Play
         </button>
-        <button className="rounded-lg py-2 px-2 mx-5 cursor-pointer bg-gray-300 w-25 text-black">
+        <button className="rounded-lg py-2 px-2 mx-5 cursor-pointer bg-gray-300/30 w-25 text-black">
           More Info
         </button>
       </div>
